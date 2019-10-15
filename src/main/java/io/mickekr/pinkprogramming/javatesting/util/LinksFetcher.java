e763ef58933d938aaa675aa777e3185d3e27bfa9
Michail Kritsotakis <michail.kritsotakis@izettle.com>
2019-10-15 16:38:04 +0200

    Remove unused imports

--- src/main/java/io/mickekr/pinkprogramming/javatesting/util/LinksFetcher.java
+++ src/main/java/io/mickekr/pinkprogramming/javatesting/util/LinksFetcher.java
@@ -1,10 +1,7 @@
 package io.mickekr.pinkprogramming.javatesting.util;
 
-import io.mickekr.pinkprogramming.javatesting.util.LinksExtractor;
-import io.mickekr.pinkprogramming.javatesting.util.UrlFetcher;
 import java.io.IOException;
 import java.util.List;
-import org.jsoup.nodes.Document;
 
 public class LinksFetcher {
     public List<String> getLinksFrom(String url) throws IOException {
